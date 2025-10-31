35034f6144020ef74466043dbc40cc78fe839e04
coder <donghaattgdtx@gmail.com>
2025-10-31 13:38:01 +0000

    dong bo lan 11

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://ocateixuzulwmrtxseom.supabase.co";
+const SUPABASE_URL = "https://ocateixuzulwmrtxseom.supabase.cohttps://eoemighznlwbhrlitfre.supabase.co";
 const SUPABASE_API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jYXRlaXh1enVsd21ydHhzZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMTYyMTQsImV4cCI6MjA3MTc5MjIxNH0.w7PLqjLGj4hNNGDh81NwDodEUCCqVSVm_PL0FpYWif8";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvZW1pZ2h6bmx3YmhybGl0ZnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0MjIwMjQsImV4cCI6MjA3Mjk5ODAyNH0.chISl1iWUEsMGSGBIVKq_EyiRm6vjTb_nH1Dt7whvns";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
