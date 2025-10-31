812e1258de6a6a8c51a2572b4470b73b2a6e49f6
coder <donghaattgdtx@gmail.com>
2025-10-31 15:02:12 +0000

    dongbo lan 17

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import "./styles.css";
-//@ts-ignore
+// 🧩 Import các trang
+// @ts-ignore
 import Layout from "./Layout";
-//@ts-ignore
+// @ts-ignore
 import Trang1 from "./Trang1";
-//@ts-ignore
+// @ts-ignore
 import Trang2 from "./Trang2";
-//@ts-ignore
+// @ts-ignore
 import ListProducts_SP from "./ListProducts_SP";
-//@ts-ignore
+// @ts-ignore
 import ProductDetail from "./ProductDetail";
 
+// 🧭 Import Router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// 🧱 App chính
 export default function App() {
   return (
     <BrowserRouter>
@@ -24,7 +26,7 @@ export default function App() {
           <Route path="trang1" element={<Trang1 />} />
           <Route path="trang2" element={<Trang2 />} />
 
-          {/* ✅ Trang chi tiết sản phẩm có hiệu ứng */}
+          {/* ✅ Trang chi tiết sản phẩm */}
           <Route path="sanpham/:id" element={<ProductDetail />} />
         </Route>
       </Routes>
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://ocateixuzulwmrtxseom.supabase.cohttps://eoemighznlwbhrlitfre.supabase.co";
+const SUPABASE_URL = "https://eoemighznlwbhrlitfre.supabase.co";
 const SUPABASE_API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvZW1pZ2h6bmx3YmhybGl0ZnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0MjIwMjQsImV4cCI6MjA3Mjk5ODAyNH0.chISl1iWUEsMGSGBIVKq_EyiRm6vjTb_nH1Dt7whvns";
 
